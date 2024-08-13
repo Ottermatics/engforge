40298d827468101dfc5c354dcd72f16519368e0f
SoundsSerious <keavin7@ottermatics.com>
2024-08-13 17:27:59 -0400

    Revert "rebuild package egg"
    This reverts commit 787084d8e83b9de7753ebc047ccf6b8186f973d7.

--- engforge/tabulation.py
+++ engforge/tabulation.py
@@ -70,8 +70,7 @@ class TabulationMixin(SolveableMixin, DataframeMixin):
         """Returns the last context"""
         raise NotImplemented("this should be implemented in the solvable class")
 
-    #@solver_cached #TODO: doesnt respond to calls on last_context
-    @property
+    @solver_cached
     def dataframe(self):
         if hasattr(self, "last_context") and hasattr(self.last_context, "dataframe"):
             return self.last_context.dataframe
